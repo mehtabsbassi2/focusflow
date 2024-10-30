7b62f0be513d918cec6069681e37e1226a03b49e
mehtabsbassi2 <148270493+mehtabsbassi2@users.noreply.github.com>
2024-10-30 09:48:06 +0000

    Tidy up

--- focusflow-backend-main/src/main/java/com/met/focusflow/repository/FeedBackRepository.java
+++
@@ -1,9 +0,0 @@
-package com.met.focusflow.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.met.focusflow.models.Feedback;
-
-public interface FeedBackRepository extends JpaRepository<Feedback,Long> {
-    
-}
